7f7be0bf61e9cb9e54c8e329fb6028a38fda14ae
Flourick <you@flourick.me>
2022-06-09 21:27:07 +0200

    1.19 dep update

--- src/main/java/net/clayborn/accurateblockplacement/AccurateBlockPlacementMod.java
+++ src/main/java/net/clayborn/accurateblockplacement/AccurateBlockPlacementMod.java
@@ -5,10 +5,12 @@ import org.lwjgl.glfw.GLFW;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 
 public class AccurateBlockPlacementMod implements ClientModInitializer
 {
@@ -25,16 +27,16 @@ public class AccurateBlockPlacementMod implements ClientModInitializer
 		KeyBinding keybind = KeyBindingHelper.registerKeyBinding(new KeyBinding("net.clayborn.accurateblockplacement.togglevanillaplacement", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "Accurate Block Placement"));
 		
 		ClientTickEvents.END_CLIENT_TICK.register(e -> {
-		    while(keybind.wasPressed()) {
+			while(keybind.wasPressed()) {
 				isAccurateBlockPlacementEnabled = !isAccurateBlockPlacementEnabled;
 				
 				MutableText message = null;
 				
 				if(isAccurateBlockPlacementEnabled) {
-					message = net.minecraft.text.Text.translatable("net.clayborn.accurateblockplacement.modplacementmodemessage");
+					message = Text.translatable("net.clayborn.accurateblockplacement.modplacementmodemessage");
 				}
 				else {
-					message = net.minecraft.text.Text.translatable("net.clayborn.accurateblockplacement.vanillaplacementmodemessage");
+					message = Text.translatable("net.clayborn.accurateblockplacement.vanillaplacementmodemessage");
 				}
 				
 				MC.inGameHud.getChatHud().addMessage(message);
